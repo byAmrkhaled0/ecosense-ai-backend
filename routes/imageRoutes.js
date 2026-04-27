85bf82886e7cfbf4e027b985782488916c58209f
Mahmoud Mansour <mahmoudmans235@gmail.com>
2026-04-28 02:54:36 +0300

    Make sensor and image routes public

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -10,7 +10,7 @@ const {
 const { protect, authorize } = require("../middleware/authMiddleware");
 const upload = require("../config/upload"); // ملف تهيئة Multer
 
-router.post("/upload", protect, upload.single("image"), uploadImage);
+router.post("/upload", upload.single("image"), uploadImage);
 router.get("/history", protect, getImageHistory);
 
 router.delete("/:id", protect, authorize("owner"), deleteImageLog);
